be677dabeb12525be8ac873b62d64ae9605764d5
Eroten <klein.damian46@gmail.com>
2022-12-08 00:17:32 +0100

    x123 wqrdsaqwe3rfsadczx

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
                 }
             }
             if (functionFirst < -2137420690) {
-                //  break;
+                  break;
             }
         }
     }
